c2f25f0beca51a6c45645eccce7b0faa59ee0707
Jeff McCoy <code@jeffm.us>
2024-07-07 03:46:27 -0500

    setup unit testing

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -14,5 +14,8 @@ export default defineConfig({
   },
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['src/setupTests.ts'],
   },
 })
