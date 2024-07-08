8a69897c2d91a00f011f44b2abd6d0afb615e73d
Jeff McCoy <code@jeffm.us>
2024-07-08 16:40:43 -0500

    one hover group for container status

--- ui/src/lib/components/data/ContainerStatus.svelte
+++ ui/src/lib/components/data/ContainerStatus.svelte
@@ -4,38 +4,50 @@
   export let containers: V1ContainerStatus[] = []
 </script>
 
-<div class="flex items-center space-x-1">
+<div class="flex items-center space-x-1 relative group h-12">
   {#each containers as c}
     <div
-      class="w-2 h-2 rounded-sm relative group"
+      class="w-2 h-2 rounded-sm"
       class:bg-green-500={c.state?.running}
       class:bg-yellow-500={c.state?.waiting}
       class:bg-gray-500={c.state?.terminated}
       class:animate-pulse={c.state?.running && !c.ready}
-    >
-      <div class="tooltip">
-        <div class="font-bold">{c.name}</div>
-        <div class="text-xs leading-loose">
-          {#if c.state?.running}
-            <div>
-              Running
-              {#if c.ready}
-                (Ready)
-              {/if}
-            </div>
-            <div>Started at: {c.state?.running?.startedAt}</div>
-          {/if}
-          {#if c.state?.waiting}
-            <div>Waiting ({c.state.waiting.reason})</div>
-          {/if}
-          {#if c.state?.terminated}
-            <div>Terminated ({c.state.terminated.reason})</div>
-            <div>Exit code: {c.state.terminated.exitCode}</div>
-            <div>Started at: {c.state.terminated.startedAt}</div>
-            <div>Finished at: {c.state.terminated.finishedAt}</div>
-          {/if}
-        </div>
-      </div>
-    </div>
+    ></div>
   {/each}
+
+  <div class="tooltip">
+    {#each containers as c}
+      <div class="font-bold">
+        <div
+          class="w-2 h-2 rounded-sm float-left mr-1 mt-1"
+          class:bg-green-500={c.state?.running}
+          class:bg-yellow-500={c.state?.waiting}
+          class:bg-gray-500={c.state?.terminated}
+          class:animate-pulse={c.state?.running && !c.ready}
+        ></div>
+        {c.name}
+      </div>
+      <div class="text-xs leading-loose">
+        {#if c.state?.running}
+          <div>
+            Running
+            {#if c.ready}
+              (Ready)
+            {/if}
+          </div>
+          <div>Started at: {c.state?.running?.startedAt}</div>
+        {/if}
+        {#if c.state?.waiting}
+          <div>Waiting ({c.state.waiting.reason})</div>
+        {/if}
+        {#if c.state?.terminated}
+          <div>Terminated ({c.state.terminated.reason})</div>
+          <div>Exit code: {c.state.terminated.exitCode}</div>
+          <div>Started at: {c.state.terminated.startedAt}</div>
+          <div>Finished at: {c.state.terminated.finishedAt}</div>
+        {/if}
+      </div>
+      <br />
+    {/each}
+  </div>
 </div>
