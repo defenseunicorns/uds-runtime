36ec7edd262fca48cac958f4452a53051a162491
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2024-07-11 13:57:12 -0600

    unit and e2e test updates

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -3,10 +3,13 @@ import type { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
   webServer: {
     command: 'npm run build && npm run preview',
-    port: 4173,
+    port: 5173,
   },
   testDir: 'tests',
-  testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+  testMatch: /(.+\.)?(spec)\.[jt]s/,
+  use: {
+    baseURL: 'http://localhost:5173/',
+  },
 }
 
 export default config
