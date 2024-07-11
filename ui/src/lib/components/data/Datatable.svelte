75202da2b428276f8bca186aaca79ec57d750717
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2024-07-10 21:38:04 -0600

    set pre-commit golangci-lint hook back to original

--- ui/src/lib/components/data/Datatable.svelte
+++ ui/src/lib/components/data/Datatable.svelte
@@ -93,7 +93,7 @@
                   <button on:click={() => rows.sortByKey(header)}>
                     {header.replaceAll('_', ' ')}
                     <AngleUpOutline
-                      class="sort 
+                      class="sort
                                   {style || ''}
                                   {$sortAsc ? 'rotate-180' : ''}
                                   {$sortBy === header ? 'opacity-100' : 'opacity-0'}"
