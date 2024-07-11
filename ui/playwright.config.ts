6d8c1142b6c6430b19a9c62dea63992b97039a77
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2024-07-11 15:22:40 -0600

    fix format; add yaml lint to workflow; install playwright

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
-   webServer: {
+  webServer: {
     command: 'npm run build && npm run preview',
     url: 'http://localhost:4173/',
   },
