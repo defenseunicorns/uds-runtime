965f13a31887d2864ff025498343989c363e8ac6
unclegedd <gedd@defenseunicorns.com>
2024-07-19 20:05:49 -0500

    optimize imports with prettier

--- ui/src/lib/features/k8s/daemonsets/store.ts
+++ ui/src/lib/features/k8s/daemonsets/store.ts
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2024-Present The UDS Authors
-
+//
 import type { V1DaemonSet as Resource } from '@kubernetes/client-node'
 
 import { type CommonRow } from '$lib/types'
