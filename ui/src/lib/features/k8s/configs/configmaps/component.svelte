39072a4404c072d878cfeadda04628e62d1072c8
Jeff McCoy <code@jeffm.us>
2024-07-23 13:07:45 -0500

    lint things

--- ui/src/lib/features/k8s/configs/configmaps/component.svelte
+++ ui/src/lib/features/k8s/configs/configmaps/component.svelte
@@ -5,7 +5,12 @@
   import { DataTable } from '$components'
   import { createStore, type Columns } from './store'
 
-  export let columns: Columns = [['name', 'emphasize'], ['namespace'], ['keys', 'line-clamp-3 max-w-screen-md'], ['age']]
+  export let columns: Columns = [
+    ['name', 'emphasize'],
+    ['namespace'],
+    ['keys', 'line-clamp-3 max-w-screen-md'],
+    ['age'],
+  ]
 </script>
 
 <DataTable {columns} {createStore} />
