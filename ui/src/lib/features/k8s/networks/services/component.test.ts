7f501e04c1fc37009e9143174a27cc4b43526de2
Darcy Cleaver <darcy@defenseunicorns.com>
2024-07-24 11:54:53 -0600

    network services view

---
+++ ui/src/lib/features/k8s/networks/services/component.test.ts
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024-Present The UDS Authors
+
+import '@testing-library/jest-dom'
+
+import { testK8sTableWithCustomColumns, testK8sTableWithDefaults } from '$features/k8s/test-helper'
+import Component from './component.svelte'
+import { createStore } from './store'
+
+suite('ServiceTable Component', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  testK8sTableWithDefaults(Component, {
+    createStore,
+    columns: [['name', 'emphasize'], ['namespace'], ['type'], ['cluster_ip'], ['external_ip'], ['ports'], ['age']],
+  })
+
+  testK8sTableWithCustomColumns(Component, { createStore })
+})
---
+++ ui/src/lib/features/k8s/networks/services/store.ts
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024-Present The UDS Authors
+
+import type { V1Service as Resource } from '@kubernetes/client-node'
+
+import { ResourceStore, transformResource } from '$features/k8s/store'
+import { type ColumnWrapper, type CommonRow, type ResourceStoreInterface } from '$features/k8s/types'
+
+interface Row extends CommonRow {
+  type: string
+  cluster_ip: string
+  external_ip: string
+  ports: string
+}
+
+export type Columns = ColumnWrapper<Row>
+
+export function createStore(): ResourceStoreInterface<Resource, Row> {
+  const url = `/api/v1/resources/networks/services`
+
+  const transform = transformResource<Resource, Row>((r) => ({
+    type: r.spec?.type ?? '',
+    cluster_ip: r.spec?.clusterIP ?? '',
+    external_ip: r.status?.loadBalancer?.ingress?.map((p) => `${p.ip}`).join(', ') ?? '',
+    ports: r.spec?.ports?.map((p) => p.nodePort ? `${p.port}:${p.nodePort}/${p.protocol}` : `${p.port}/${p.protocol}`).join(', ') ?? '',
+  }))
+
+  const store = new ResourceStore<Resource, Row>('name')
+
+  return {
+    ...store,
+    start: () => store.start(url, transform),
+    sortByKey: store.sortByKey.bind(store),
+  }
+}
