1d0b68aa39ad59e79107ddb61d650ae427cc9598
decleaver <85503726+decleaver@users.noreply.github.com>
2024-07-25 01:24:41 -0600

    feat(ui): networks-services view (#61)

---
+++ ui/src/lib/features/k8s/networks/services/store.ts
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024-Present The UDS Authors
+
+import type { V1Service as Resource } from '@kubernetes/client-node'
+
+import { ResourceStore, transformResource } from '$features/k8s/store'
+import { type ColumnWrapper, type CommonRow, type ResourceStoreInterface } from '$features/k8s/types'
+
+interface Row extends CommonRow {
+  type: string
+  cluster_ip: string
+  external_ip: string
+  ports: string
+  status: string
+}
+
+export type Columns = ColumnWrapper<Row>
+
+export function createStore(): ResourceStoreInterface<Resource, Row> {
+  const url = `/api/v1/resources/networks/services`
+
+  const transform = transformResource<Resource, Row>((r) => ({
+    type: r.spec?.type ?? '',
+    cluster_ip: r.spec?.clusterIP ?? '',
+    external_ip: r.status?.loadBalancer?.ingress?.map((p) => `${p.ip}`).join(', ') ?? '-',
+    ports:
+      r.spec?.ports
+        ?.map((p) => (p.nodePort ? `${p.port}:${p.nodePort}/${p.protocol}` : `${p.port}/${p.protocol}`))
+        .join(', ') ?? '',
+    status: isInSuccessState(r) ? 'Succeeded' : 'Pending',
+  }))
+
+  const store = new ResourceStore<Resource, Row>('name')
+
+  return {
+    ...store,
+    start: () => store.start(url, transform),
+    sortByKey: store.sortByKey.bind(store),
+  }
+}
+
+/**
+ * Success state of a Service depends on the type of service
+ * https://kubernetes.io/docs/concepts/services-networking/service/#publishing-services-service-types
+ * ClusterIP:     ClusterIP is defined
+ * NodePort:      ClusterIP is defined
+ * LoadBalancer:  ClusterIP is defined __and__ external endpoints exist
+ * ExternalName:  true
+ */
+function isInSuccessState(resource: Resource): boolean {
+  const resourceType = resource.spec?.type
+  switch (resourceType) {
+    case 'ExternalName':
+      return true
+    case 'LoadBalancer':
+      if (resource.status?.loadBalancer?.ingress?.length === 0) {
+        return false
+      }
+      break
+    case 'ClusterIP':
+    case 'NodePort':
+    default:
+      break
+  }
+  return resource.spec?.clusterIPs?.length ? resource.spec?.clusterIPs?.length > 0 : false
+}
