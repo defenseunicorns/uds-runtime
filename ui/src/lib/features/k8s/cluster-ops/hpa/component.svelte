6509598b632fac25f18f3270b4cdfb2cc318d59d
Billy Figueroa <billy@defenseunicorns.com>
2024-07-26 15:15:24 -0400

    feat: adding more fields

--- ui/src/lib/features/k8s/cluster-ops/hpa/component.svelte
+++ ui/src/lib/features/k8s/cluster-ops/hpa/component.svelte
@@ -5,7 +5,15 @@
   import { DataTable } from '$components'
   import { createStore, type Columns } from './store'
 
-  export let columns: Columns = [['name', 'emphasize'], ['namespace'], ['replicas'], ['age']]
+  export let columns: Columns = [
+    ['name', 'emphasize'],
+    ['namespace'],
+    ['min_pods'],
+    ['max_pods'],
+    ['replicas'],
+    ['age'],
+    ['status'],
+  ]
 </script>
 
 <DataTable {columns} {createStore} />
--- ui/src/lib/features/k8s/cluster-ops/hpa/store.ts
+++ ui/src/lib/features/k8s/cluster-ops/hpa/store.ts
@@ -1,25 +1,34 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2024-Present The UDS Authors
 
-import type { V1HorizontalPodAutoscaler as Resource } from '@kubernetes/client-node'
+import type { V2HorizontalPodAutoscaler as Resource } from '@kubernetes/client-node'
 
 import { ResourceStore, transformResource } from '$features/k8s/store'
 import { type ColumnWrapper, type CommonRow, type ResourceStoreInterface } from '$features/k8s/types'
 
 interface Row extends CommonRow {
-  replicas: string
+  min_pods?: number
+  max_pods?: number
+  replicas?: number
+  status: string
 }
 
 export type Columns = ColumnWrapper<Row>
 
 export function createStore(): ResourceStoreInterface<Resource, Row> {
-  const url = `/api/v1/resources/cluster-ops/hpas`
+  const url = `/api/v1/resources/cluster-ops/hpas?dense=true`
 
   const transform = transformResource<Resource, Row>((r) => {
     console.log('r')
     console.log(r)
+
+    const status = r.status?.conditions?.filter((c) => c.status === 'True')[0]?.type
+    // console.log(r.status?.conditions?.filter((c) => c.status === 'True'))
     return {
+      min_pods: r.spec?.minReplicas,
+      max_pods: r.spec?.maxReplicas,
       replicas: r.status?.currentReplicas,
+      status,
     }
   })
 
