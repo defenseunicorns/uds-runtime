a859b7c9f1ab6f1239b4e3833b567c36dffaeb0e
Billy Figueroa <billy@defenseunicorns.com>
2024-07-25 23:48:48 -0400

    refactor: removing unnecessary fields

--- ui/src/lib/features/k8s/cluster-ops/limit-ranges/store.ts
+++ ui/src/lib/features/k8s/cluster-ops/limit-ranges/store.ts
@@ -4,22 +4,14 @@
 import type { V1LimitRange as Resource } from '@kubernetes/client-node'
 
 import { ResourceStore, transformResource } from '$features/k8s/store'
-import { type ColumnWrapper, type CommonRow, type ResourceStoreInterface } from '$features/k8s/types'
+import { type CommonRow, type ResourceStoreInterface } from '$features/k8s/types'
 
-interface Row extends CommonRow {
-  namespace?: string
-}
-
-export type Columns = ColumnWrapper<Row>
-
-export function createStore(): ResourceStoreInterface<Resource, Row> {
+export function createStore(): ResourceStoreInterface<Resource, CommonRow> {
   const url = `/api/v1/resources/cluster-ops/limit-range-classes`
 
-  const transform = transformResource<Resource, Row>((r) => ({
-    namespace: r.metadata?.namespace ?? '',
-  }))
+  const transform = transformResource<Resource, CommonRow>(() => ({}))
 
-  const store = new ResourceStore<Resource, Row>('name')
+  const store = new ResourceStore<Resource, CommonRow>('name')
 
   return {
     ...store,
