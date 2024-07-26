abcf75695647363488d70e8212e254b66f717a14
Billy Figueroa <billy@defenseunicorns.com>
2024-07-26 15:31:22 -0400

    fix: removing unnecessary fields

--- ui/src/lib/features/k8s/cluster-ops/resource-quotas/store.ts
+++ ui/src/lib/features/k8s/cluster-ops/resource-quotas/store.ts
@@ -4,22 +4,14 @@
 import type { V1ResourceQuota as Resource } from '@kubernetes/client-node'
 
 import { ResourceStore, transformResource } from '$features/k8s/store'
-import { type ColumnWrapper, type CommonRow, type ResourceStoreInterface } from '$features/k8s/types'
+import { type CommonRow, type ResourceStoreInterface } from '$features/k8s/types'
 
-interface Row extends CommonRow {
-  handler: string
-}
-
-export type Columns = ColumnWrapper<Row>
-
-export function createStore(): ResourceStoreInterface<Resource, Row> {
+export function createStore(): ResourceStoreInterface<Resource, CommonRow> {
   const url = `/api/v1/resources/cluster-ops/resource-quotas`
 
-  const transform = transformResource<Resource, Row>((r) => ({
-    handler: r.metadata?.namespace ?? '',
-  }))
+  const transform = transformResource<Resource, CommonRow>(() => ({}))
 
-  const store = new ResourceStore<Resource, Row>('name')
+  const store = new ResourceStore<Resource, CommonRow>('name')
 
   return {
     ...store,
