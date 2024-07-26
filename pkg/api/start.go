a4f9226af391c58e65c08350c321092a97362629
Billy Figueroa <billy@defenseunicorns.com>
2024-07-25 23:53:52 -0400

    fix: fixing format issue

--- pkg/api/start.go
+++ pkg/api/start.go
@@ -106,7 +106,8 @@ func Start(assets embed.FS) error {
 				r.Get("/runtime-classes/{uid}", sse.Bind(cache.RuntimeClasses))
 
 				r.Get("/resource-quotas", sse.Bind(cache.ResourceQuotas))
-				r.Get("/resource-quotas/{uid}", sse.Bind(cache.ResourceQuotas))			})
+				r.Get("/resource-quotas/{uid}", sse.Bind(cache.ResourceQuotas))
+			})
 
 			// Network resources
 			r.Route("/networks", func(r chi.Router) {
