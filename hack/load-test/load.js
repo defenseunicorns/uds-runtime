2fa75e2b98328c99f305d048f72c7ff64463ace8
unclegedd <gedd@defenseunicorns.com>
2024-07-29 10:17:20 -0500

    refactor load test script

--- hack/load-test/load.js
+++ hack/load-test/load.js
@@ -1,27 +1,52 @@
 import { K8s, kind } from 'kubernetes-fluent-client'
 
-const namespace = 'load-test-' + Math.floor(Math.random() * 1000)
+const namespace = 'load-test' + Math.floor(Math.random() * 1000);
 
-K8s(kind.Namespace).Apply({
-  metadata: {
-    name: namespace,
-  },
-})
-
-for (let i = 0; i < 100; i++) {
-  // Create the Pod in the namespace
-  K8s(kind.Pod).Apply({
-    metadata: {
-      name: 'nginx-load-' + Math.floor(Math.random() * 1000),
-      namespace,
-    },
-    spec: {
-      containers: [
-        {
-          name: 'my-container',
-          image: 'nginx',
-        },
-      ],
-    },
-  })
+async function createNamespace() {
+  try {
+    await K8s(kind.Namespace).Apply({
+      metadata: {
+        name: namespace,
+      },
+    });
+    console.log(`Namespace ${namespace} created`);
+  } catch (error) {
+    console.error(`Error creating namespace ${namespace}:`, error);
+  }
 }
+
+async function createPod(index) {
+  try {
+    const podName = 'nginx-load-' + Math.floor(Math.random() * 1000);
+    await K8s(kind.Pod).Apply({
+      metadata: {
+        name: podName,
+        namespace,
+      },
+      spec: {
+        containers: [
+          {
+            name: 'my-container',
+            image: 'nginx',
+          },
+        ],
+      },
+    });
+    console.log(`Pod ${podName} created`);
+  } catch (error) {
+    console.error(`Error creating pod ${index}:`, error);
+  }
+}
+
+async function createPods() {
+  await createNamespace();
+  for (let i = 0; i < 100; i++) {
+    await createPod(i);
+  }
+}
+
+createPods().then(() => {
+  console.log('All pods created');
+}).catch((error) => {
+  console.error('Error creating pods:', error);
+});
