a55696defbdf7215f752fc36a8d666b6995476f0
Billy Figueroa <billy@defenseunicorns.com>
2024-07-30 03:45:30 -0400

    feat(ui): add cluster ops hpa k8s view (#97)
    ## Description
    Adding cluster ops HPAs view
    
    ## Related Issue
    
    - #46

---
+++ ui/src/lib/features/k8s/cluster-ops/hpa/component.test.ts
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024-Present The UDS Authors
+
+import '@testing-library/jest-dom'
+
+import { testK8sTableWithCustomColumns, testK8sTableWithDefaults } from '$features/k8s/test-helper'
+import Component from './component.svelte'
+import { createStore } from './store'
+
+suite('PriorityClassesTable Component', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  testK8sTableWithDefaults(Component, {
+    createStore,
+    columns: [
+      ['name', 'emphasize'],
+      ['namespace'],
+      ['metrics'],
+      ['min_pods'],
+      ['max_pods'],
+      ['replicas'],
+      ['age'],
+      ['status'],
+    ],
+  })
+
+  testK8sTableWithCustomColumns(Component, { createStore })
+})
---
+++ ui/src/lib/features/k8s/cluster-ops/hpa/store.ts
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024-Present The UDS Authors
+
+import type { V2HorizontalPodAutoscaler as Resource } from '@kubernetes/client-node'
+
+import { ResourceStore, transformResource } from '$features/k8s/store'
+import { type ColumnWrapper, type CommonRow, type ResourceStoreInterface } from '$features/k8s/types'
+
+interface Row extends CommonRow {
+  metrics?: string
+  min_pods?: number
+  max_pods?: number
+  replicas?: number
+  status: string
+}
+
+export type Columns = ColumnWrapper<Row>
+
+export function createStore(): ResourceStoreInterface<Resource, Row> {
+  const url = `/api/v1/resources/cluster-ops/hpas?dense=true`
+
+  const transform = transformResource<Resource, Row>((r) => {
+    const status = r.status?.conditions
+      ?.filter((c) => c.status === 'True')
+      .map((c) => c.type)
+      .join(' ')
+
+    const HPAUtilization = `${r.status?.currentMetrics?.at(0)?.resource?.current.averageUtilization}%`
+
+    return {
+      metrics: `${HPAUtilization || 'unknown'} / ${r.spec?.metrics?.at(0)?.resource?.target?.averageUtilization}%`,
+      min_pods: r.spec?.minReplicas,
+      max_pods: r.spec?.maxReplicas,
+      replicas: r.status?.currentReplicas,
+      status,
+    }
+  })
+
+  const store = new ResourceStore<Resource, Row>('name')
+
+  return {
+    ...store,
+    start: () => store.start(url, transform),
+    sortByKey: store.sortByKey.bind(store),
+  }
+}
