cad4f3508fe301532a71e4bd04986f7113df406d
UncleGedd <42304551+UncleGedd@users.noreply.github.com>
2024-07-31 08:03:28 -0500

    feat(ui): adds validatingwebhooks view (#110)
    ## Description
    
    Adds validatingwebhooks view

---
+++ ui/src/lib/features/k8s/cluster-ops/validatingwebhooks/store.ts
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024-Present The UDS Authors
+
+import type { V1ValidatingWebhookConfiguration as Resource } from '@kubernetes/client-node'
+
+import { ResourceStore, transformResource } from '$features/k8s/store'
+import { type ColumnWrapper, type CommonRow, type ResourceStoreInterface } from '$features/k8s/types'
+
+export interface Row extends CommonRow {
+  webhooks: string
+}
+
+export type Columns = ColumnWrapper<Row>
+
+export function createStore(): ResourceStoreInterface<Resource, Row> {
+  const url = `/api/v1/resources/cluster-ops/validatingwebhooks?dense=true`
+
+  const transform = transformResource<Resource, Row>((r) => ({
+    webhooks:
+      r.webhooks
+        ?.map((w) => w.name)
+        .sort()
+        .join(', ') ?? '',
+  }))
+
+  const store = new ResourceStore<Resource, Row>('name', true)
+
+  return {
+    ...store,
+    start: () => store.start(url, transform),
+    sortByKey: store.sortByKey.bind(store),
+  }
+}
