75a72b0e5f19c98ab96e2fbc9cd9dd3c5a708d40
Billy Figueroa <billy@defenseunicorns.com>
2024-07-31 11:00:29 -0400

    chore: adding license info to files

--- ui/src/lib/components/Drawer/Row.svelte
+++ ui/src/lib/components/Drawer/Row.svelte
@@ -1,3 +1,6 @@
+<!-- SPDX-License-Identifier: Apache-2.0 -->
+<!-- SPDX-FileCopyrightText: 2024-Present The UDS Authors -->
+
 <script context="module" lang="ts">
   export type VariantType = 'text' | 'icon-text' | 'external-link' | 'internal-link' | 'input-copy' | 'badges'
 </script>
--- ui/src/lib/components/Drawer/RowItem.svelte
+++ ui/src/lib/components/Drawer/RowItem.svelte
@@ -1,3 +1,6 @@
+<!-- SPDX-License-Identifier: Apache-2.0 -->
+<!-- SPDX-FileCopyrightText: 2024-Present The UDS Authors -->
+
 <script lang="ts">
   import { Db2Database } from 'carbon-icons-svelte'
 
