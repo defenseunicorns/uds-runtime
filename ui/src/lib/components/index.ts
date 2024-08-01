32cd2c2667d28396e8f2f6af1aca65a1380bf609
Billy Figueroa <billy@defenseunicorns.com>
2024-08-01 14:44:13 -0400

    fix: removing exports

--- ui/src/lib/components/index.ts
+++ ui/src/lib/components/index.ts
@@ -4,5 +4,3 @@
 export { default as AnsiDisplay } from './AnsiDisplay/component.svelte'
 export { default as DataTable } from './k8s/DataTable/component.svelte'
 export { default as Drawer } from './k8s/Drawer/component.svelte'
-export { default as Row } from './k8s/Drawer/Row.svelte'
-export { default as RowItem } from './k8s/Drawer/RowItem.svelte'
