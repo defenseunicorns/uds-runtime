e7cb2c6c208ed387af361e807937216f457ff51f
Billy Figueroa <billy@defenseunicorns.com>
2024-08-01 15:46:47 -0400

    fix: removing unused import

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,8 +1,4 @@
 <!-- SPDX-License-Identifier: Apache-2.0 -->
 <!-- SPDX-FileCopyrightText: 2024-Present The UDS Authors -->
 
-<script lang="ts">
-  import Row from '$components/k8s/Drawer/Row.svelte'
-</script>
-
 <h1 class="text-white text-5xl mb-7">Home</h1>
