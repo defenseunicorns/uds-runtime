2e80fc60b229b50ce879b8c0ed328347c6eb4c38
Jeff McCoy <code@jeffm.us>
2024-08-02 05:10:00 -0500

    cleanup

--- ui/src/routes/monitor/events/+page.svelte
+++
@@ -1,8 +0,0 @@
-<!-- SPDX-License-Identifier: Apache-2.0 -->
-<!-- SPDX-FileCopyrightText: 2024-Present The UDS Authors -->
-
-<script lang="ts">
-  import { EventTable } from '$features/k8s'
-</script>
-
-<EventTable />
