3740d9ef92236e7f28468186869ac665de839d08
Jeff McCoy <code@jeffm.us>
2024-08-02 05:06:57 -0500

    add LInk component

---
+++ ui/src/lib/components/Link/component.svelte
@@ -0,0 +1,16 @@
+<!-- SPDX-License-Identifier: Apache-2.0 -->
+<!-- SPDX-FileCopyrightText: 2024-Present The UDS Authors -->
+
+<script lang="ts">
+  export let href: string
+  export let target: string = '_blank'
+  export let text: string
+</script>
+
+<a
+  {href}
+  {target}
+  on:click|stopPropagation
+  rel="noopener noreferrer"
+  class="font-medium text-blue-600 dark:text-blue-500 hover:underline pr-4">{text}</a
+>
