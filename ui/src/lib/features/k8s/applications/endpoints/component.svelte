32f84d310fc1f83cb6d10816af9e1c44d0d101b3
Jeff McCoy <code@jeffm.us>
2024-08-02 05:40:20 -0500

    add application endpoint view

---
+++ ui/src/lib/features/k8s/applications/endpoints/component.svelte
@@ -0,0 +1,14 @@
+<!-- SPDX-License-Identifier: Apache-2.0 -->
+<!-- SPDX-FileCopyrightText: 2024-Present The UDS Authors -->
+
+<script lang="ts">
+  import { DataTable } from '$components'
+  import { createStore, type Columns } from './store'
+
+  export let columns: Columns = [['url', 'emphasize', 'link-external'], ['namespace'], ['status'], ['age']]
+
+  const name = 'Application Endpoints'
+  const description = 'Application Endpoints are exposed by the UDS Operator via a UDS Package CR.'
+</script>
+
+<DataTable {columns} {createStore} {name} {description} disableRowClick={true} />
