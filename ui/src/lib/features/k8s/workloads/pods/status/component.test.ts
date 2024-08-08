28eb652c8ce49a7658a3532bf1ed2e7937d0498d
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2024-08-07 20:45:13 -0600

    fix: pod status color

---
+++ ui/src/lib/features/k8s/workloads/pods/status/component.test.ts
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/svelte'
+import Status from './component.svelte'
+
+describe('Status component', () => {
+  test('renders text-green-400 for Running', () => {
+    const { container } = render(Status, { props: { status: 'Running' } })
+    expect(container.firstChild).toHaveClass('text-green-400')
+  })
+  test('renders text-red-400 for Running', () => {
+    const { container } = render(Status, { props: { status: 'Stopped' } })
+    expect(container.firstChild).toHaveClass('text-red-400')
+  })
+  test('renders text-yellow-500 for Running', () => {
+    const { container } = render(Status, { props: { status: 'Pending' } })
+    expect(container.firstChild).toHaveClass('text-orange-300')
+  })
+})
