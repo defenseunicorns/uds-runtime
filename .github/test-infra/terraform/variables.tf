2ba9e261255c57b1631249e3aa1864eb5c7b34f1
Tristan Holaday <40547442+TristanHoladay@users.noreply.github.com>
2024-08-16 12:05:52 -0600

    chore: add test infra for dummy cluster (#157)
    Co-authored-by: UncleGedd <42304551+UncleGedd@users.noreply.github.com>
    Co-authored-by: Billy Figueroa <billy@defenseunicorns.com>
    Co-authored-by: decleaver <85503726+decleaver@users.noreply.github.com>

---
+++ .github/test-infra/terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  description = "The AWS region to deploy the EC2 instance"
+  type        = string
+  default    = "us-west-2"
+}
+
+variable "permissions_boundary_name" {
+  description = "The name of the permissions boundary to attach to the IAM role"
+  type        = string
+}
+
+variable "permissions_boundary_arn" {
+  description = "The ARN of the permissions boundary to attach to the IAM role"
+  type        = string
+}
+
+variable "enable_ssh" {
+  description = "Enable SSH access to the EC2 instance"
+  type        = bool
+  default     = false
+}
+
+variable "ssh_ip" {
+  description = "The IP address to allow SSH access from"
+  type        = string
+  default    = ""
+}
