f797697412b211f7482b02a9d4722fe5bf657b25
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2024-08-21 12:47:37 -0600

    run go tests based on build tags

--- pkg/pepr/stream_test.go
+++ pkg/pepr/stream_test.go
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2024-Present The UDS Authors
 
+//go:build unit
+
 // Package pepr contains the logic monitoring Pepr operations in a UDS Cluster
 package pepr
 
