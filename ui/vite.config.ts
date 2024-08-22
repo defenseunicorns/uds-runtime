966b6b24564446a28d31dcd777f9c84f12af1ccc
Darcy Cleaver <darcy@defenseunicorns.com>
2024-08-22 14:56:51 -0600

    docs

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig(({ mode }) => ({
         changeOrigin: true,
       },
       '/config': {
-        // Add this new proxy entry
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
