18da1c3131d1263001f791d8a6b179cfc9ff78d1
Darcy Cleaver <darcy@defenseunicorns.com>
2024-08-23 12:35:37 -0600

    cleanup

--- ui/tests/api-auth.spec.ts
+++ ui/tests/api-auth.spec.ts
@@ -1,68 +1,40 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2024-Present The UDS Authors
 
-// Test file prefixed with "z-" to ensure it runs last since it stops the server and restarts it
 import { expect, test } from '@playwright/test'
 import { ChildProcess, exec } from 'child_process'
-import * as net from 'net'
 
 let serverProcess: ChildProcess
 const serverLogs: string[] = []
 let extractedToken: string | null = null
 
-function isPortInUse(port: number): Promise<boolean> {
-  return new Promise((resolve) => {
-    const server = net
-      .createServer()
-      .once('error', () => resolve(true))
-      .once('listening', () => {
-        server.close()
-        resolve(false)
-      })
-      .listen(port)
-  })
-}
-
 test.beforeAll(async () => {
-  // await killProcessOnPort(port.toString())
-  // Check what is running on port 8080
-  try {
-    const inUse = await isPortInUse(8080)
-    if (inUse) {
-      console.log('Port 8080 is in use')
-    } else {
-      console.log('Port 8080 is not in use')
-    }
-  } catch (error) {
-    console.error(`Error checking port 8080: ${error}`)
-  }
-
   // Start the server
-  await new Promise<void>((resolve, reject) => {
-    console.time('Server Start Time') // Start the timer
-
+  return new Promise<void>((resolve, reject) => {
     serverProcess = exec('VITE_API_AUTH=true ../build/uds-runtime', (error) => {
       if (error) {
         console.error(`Error starting server: ${error}`)
-        console.timeEnd('Server Start Time')
-        reject(error)
-        return
       }
     })
 
-    // Capture stdout
-    if (serverProcess && serverProcess.stdout) {
-      serverProcess.stdout.on('data', (data) => {
+    if (serverProcess && serverProcess.stderr) {
+      serverProcess.stderr.on('data', (data) => {
         const log = data.toString()
-        serverLogs.push(`stdout: ${log}`)
-        resolve() // Resolve the promise after pushing to serverLogs
+        console.error(`stderr: ${log}`)
+        serverLogs.push(`stderr: ${log}`)
         extractToken(log)
+        resolve()
       })
     }
-  })
 
-  // Wait for the server to be ready
-  await new Promise((resolve) => setTimeout(resolve, 10000)) // Adjust the timeout as needed
+    // Handle process exit
+    serverProcess.on('exit', (code) => {
+      console.log(`Server process exited with code ${code}`)
+      if (code !== 0) {
+        reject(new Error(`Server process exited with code ${code}`))
+      }
+    })
+  })
 })
 
 test.afterAll(async () => {
@@ -72,22 +44,20 @@ test.afterAll(async () => {
   }
 })
 
-// async function killProcessOnPort(port: string) {
-//   const list = await find('port', port)
-//   list.forEach((proc) => {
-//     try {
-//       process.kill(proc.pid)
-//     } catch (e) {
-//       console.error(`Failed to kill process ${proc.pid}: ${e.message}`)
-//     }
-//   })
-// }
-
 function extractToken(log: string) {
   const match = log.match(/auth\?token=([^&\s]+)/)
   if (match) {
     extractedToken = match[1]
+    // ANSI escape codes are being appended to the token
+    extractedToken = stripAnsiCodes(extractedToken)
+    console.log(`Extracted token: ${extractedToken}`)
   }
+}
+
+function stripAnsiCodes(str: string): string {
+  // This regex matches all ANSI escape codes
+  const ansiRegex = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g
+  return str.replace(ansiRegex, '')
 }
 
 test.describe.serial('Authentication Tests', () => {
