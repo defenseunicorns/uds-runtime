ab48a41f3c6f78adf80bf32de42cda7a6f97482d
Darcy Cleaver <darcy@defenseunicorns.com>
2024-08-27 13:28:14 -0600

    update playwright config for disabled auth

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -6,7 +6,7 @@ const port = VITE_PORT_ENV ?? '8080'
 
 export default defineConfig({
   webServer: {
-    command: '../build/uds-runtime',
+    command: 'API_AUTH_DISABLED=true ../build/uds-runtime',
     url: `http://localhost:${port}`,
     reuseExistingServer: !process.env.CI,
   },
