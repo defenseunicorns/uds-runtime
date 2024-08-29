f2eb438f2433d2d726d5e0f5823b245cbd6ecb6f
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2024-08-29 10:58:25 -0600

    chore(api): expand api tests

--- pkg/test/api_test.go
+++ pkg/test/api_test.go
@@ -8,6 +8,7 @@ package test
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -78,6 +79,7 @@ func TestQueryParams(t *testing.T) {
 			var data []map[string]interface{}
 			err = json.Unmarshal(rr.Body.Bytes()[keyIndx:], &data)
 			require.NoError(t, err)
+			require.GreaterOrEqual(t, len(data), 1)
 
 			// Assert dense versus sparse
 			if tt.isDense {
@@ -91,6 +93,93 @@ func TestQueryParams(t *testing.T) {
 				require.Equal(t, 1, len(data))
 				require.Equal(t, data[0]["metadata"].(map[string]interface{})["namespace"], "podinfo")
 				require.Contains(t, data[0]["metadata"].(map[string]interface{})["name"], "podinfo")
+			}
+		})
+	}
+}
+
+// TODO: Add case for no ns with UID
+
+func TestFieldSelectors(t *testing.T) {
+	r, err := api.Setup(nil)
+	require.NoError(t, err)
+
+	uid := ""
+
+	tests := []struct {
+		name    string
+		url     string
+		isDense bool
+	}{
+		{
+			name: "once=true",
+			url:  "/api/v1/resources/workloads/pods?once=true&fields=.metadata.name,.metadata.uid,spec.nodeName",
+		},
+		{
+			name:    "sse",
+			url:     "/api/v1/resources/workloads/pods?fields=metadata.name,metadata.uid,spec.nodeName",
+			isDense: true,
+		},
+		{
+			name: "sse namespace & name",
+			url:  "/api/v1/resources/workloads/pods?namespace=podinfo&name=podinfo&fields=metadata.name,metadata.uid,spec.nodeName",
+		},
+		{
+			name: "uid",
+			url:  "/api/v1/resources/workloads/pods/{uid}?fields=metadata.name,metadata.uid,spec.nodeName",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			keyIndx := 5
+			if strings.Contains(tt.url, "once=true") || strings.Contains(tt.url, "{uid}") {
+				keyIndx = 0
+			}
+
+			// Create a new context with a timeout
+			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+			defer cancel()
+
+			rr := httptest.NewRecorder()
+			tt.url = strings.Replace(tt.url, "{uid}", uid, 1)
+			req := httptest.NewRequest("GET", tt.url, nil)
+
+			// Start serving the request for 1 second
+			go func(ctx context.Context) {
+				r.ServeHTTP(rr, req)
+			}(ctx)
+
+			// wait for the context to be done
+			<-ctx.Done()
+
+			fmt.Println(rr.Body.String())
+
+			var data []map[string]interface{}
+			var resource map[string]interface{}
+
+			if strings.Contains(tt.url, uid) && uid != "" {
+				json.Unmarshal(rr.Body.Bytes()[keyIndx:], &resource)
+			} else {
+				json.Unmarshal(rr.Body.Bytes()[keyIndx:], &data)
+				resource = data[0]
+			}
+
+			// Assert fields selection
+			require.Equal(t, 2, len(resource))
+			require.NotNil(t, resource["metadata"].(map[string]interface{})["name"])
+			require.NotNil(t, resource["spec"].(map[string]interface{})["nodeName"])
+			require.NotNil(t, resource["metadata"].(map[string]interface{})["uid"])
+
+			uid = resource["metadata"].(map[string]interface{})["uid"].(string)
+
+			// Assert namespace and name filtering
+			if strings.Contains(tt.url, "namespace=podinfo&name=podinfo") {
+				require.Equal(t, 1, len(data))
+				require.Equal(t, 2, len(resource))
+				require.Contains(t, resource["metadata"].(map[string]interface{})["name"], "podinfo")
+				require.NotNil(t, resource["spec"].(map[string]interface{})["nodeName"])
+				require.NotNil(t, resource["metadata"].(map[string]interface{})["uid"])
 			}
 		})
 	}
