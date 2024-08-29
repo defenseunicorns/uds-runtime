d0403eefa4faf614343508eff87afb7c8dc2ecbc
Billy Figueroa <billy@defenseunicorns.com>
2024-08-29 13:42:40 -0400

    fix(ui): fixing unit test

--- ui/src/lib/features/k8s/workloads/pods/status/component.test.ts
+++ ui/src/lib/features/k8s/workloads/pods/status/component.test.ts
@@ -6,11 +6,11 @@ describe('Status component', () => {
     const { container } = render(Status, { props: { status: 'Running' } })
     expect(container.firstChild).toHaveClass('text-green-400')
   })
-  test('renders text-red-400 for Running', () => {
-    const { container } = render(Status, { props: { status: 'Stopped' } })
+  test('renders text-red-400 for Failed', () => {
+    const { container } = render(Status, { props: { status: 'Failed' } })
     expect(container.firstChild).toHaveClass('text-red-400')
   })
-  test('renders text-yellow-500 for Running', () => {
+  test('renders text-yellow-500 for Pending', () => {
     const { container } = render(Status, { props: { status: 'Pending' } })
     expect(container.firstChild).toHaveClass('text-orange-300')
   })
