4ed8b2ee3602ed9918537539306daa91bcccfeaf
Billy Figueroa <billy@defenseunicorns.com>
2024-09-23 13:09:31 -0400

    fix: fixing memory bar for overview page

--- ui/src/lib/components/ProgressBar/component.svelte
+++ ui/src/lib/components/ProgressBar/component.svelte
@@ -32,7 +32,7 @@
 <div class="flex flex-col">
   <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 mt-3">
     <div
-      class={`dark:bg-${unit === 'GB' ? 'green' : 'blue'}-600 rounded-full ${sizeMapping[size]}`}
+      class={`${unit === 'GB' ? 'bg-green-500' : 'dark:bg-blue-600'} rounded-full ${sizeMapping[size]}`}
       style={`width: ${calculatedWidth}%`}
     ></div>
   </div>
