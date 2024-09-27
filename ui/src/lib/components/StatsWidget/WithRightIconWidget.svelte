61ebba71f9467fb96ea494d2b92ce87d6e5460f1
Billy Figueroa <billy@defenseunicorns.com>
2024-09-27 11:54:34 -0400

    feat(ui): adding themeing to the icon for with right icon widget

--- ui/src/lib/components/StatsWidget/WithRightIconWidget.svelte
+++ ui/src/lib/components/StatsWidget/WithRightIconWidget.svelte
@@ -3,17 +3,37 @@
   import { Card } from '$components'
   import { type CarbonIcon } from 'carbon-icons-svelte'
 
+  type IconThemeType = 'primary' | 'warning' | 'critical'
+
+  const themeMapping = {
+    primary: {
+      bgColor: 'bg-blue-900',
+      iconColor: '#3f83f8',
+    },
+    warning: {
+      bgColor: 'bg-yellow-100',
+      iconColor: '#E3A008',
+    },
+    critical: {
+      bgColor: 'bg-pink-200',
+      iconColor: '#F98080',
+    },
+  } as const
+
   export let statText: string
   export let helperText: string
   export let icon: typeof CarbonIcon
   export let link: string = ''
+  export let iconTheme: IconThemeType = 'primary'
+
+  $: iconThemeOption = themeMapping[iconTheme]
 </script>
 
 <Card>
   <button on:click={() => goto(link)}>
     <div class="flex items-center justify-start space-x-4">
-      <div class="bg-blue-900 p-3 rounded-md">
-        <svelte:component this={icon} size={24} color="#3f83f8" />
+      <div class="{iconThemeOption['bgColor']} p-3 rounded-md">
+        <svelte:component this={icon} size={24} color={iconThemeOption['iconColor']} />
       </div>
 
       <div class="flex flex-col items-start">
