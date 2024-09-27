86b2ea2015e8476c9be63ae4c6b4c70710f52465
Billy Figueroa <billy@defenseunicorns.com>
2024-09-27 12:19:04 -0400

    chore: updating light theme colors for widgets

--- ui/src/lib/components/StatsWidget/WithRightIconWidget.svelte
+++ ui/src/lib/components/StatsWidget/WithRightIconWidget.svelte
@@ -7,15 +7,18 @@
 
   const themeMapping = {
     primary: {
-      bgColor: 'bg-blue-900',
+      bgColor: 'bg-blue-200',
+      bgColorDark: 'dark:bg-blue-900',
       iconColor: '#3f83f8',
     },
     warning: {
       bgColor: 'bg-yellow-100',
+      bgColorDark: 'bg-yellow-100',
       iconColor: '#E3A008',
     },
     critical: {
       bgColor: 'bg-pink-200',
+      bgColorDark: 'bg-pink-200',
       iconColor: '#F98080',
     },
   } as const
@@ -32,18 +35,18 @@
 <Card>
   <button on:click={() => goto(link)}>
     <div class="flex items-center justify-start space-x-4">
-      <div class="{iconThemeOption['bgColor']} p-3 rounded-md">
+      <div class="{iconThemeOption['bgColor']} {iconThemeOption['bgColorDark']} p-3 rounded-md">
         <svelte:component this={icon} size={24} color={iconThemeOption['iconColor']} />
       </div>
 
       <div class="flex flex-col items-start">
         <dt
-          class="text-3xl font-semibold text-blue-500 dark:text-white truncate"
+          class="text-3xl font-semibold text-black dark:text-white truncate"
           data-testid={`resource-count-${helperText.split(' ')[0].toLowerCase()}`}
         >
           {statText}
         </dt>
-        <dd class="mt-1 text-sm text-gray-900 dark:text-gray-400">
+        <dd class="mt-1 text-sm text-gray-500 dark:text-gray-400">
           {helperText}
         </dd>
       </div>
