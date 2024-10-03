c9485cf9780f954d4a9120a5e32f22adf9c958b3
Billy Figueroa <billy@defenseunicorns.com>
2024-10-03 16:01:15 -0400

    feat: adding footer with link to events

--- ui/src/lib/features/k8s/cluster-overview/component.svelte
+++ ui/src/lib/features/k8s/cluster-overview/component.svelte
@@ -5,7 +5,7 @@
   import { onMount } from 'svelte'
 
   import { ProgressBarWidget, WithRightIconWidget } from '$components'
-  import { Analytics, DataVis_1 } from 'carbon-icons-svelte'
+  import { Analytics, ChevronRight, DataVis_1 } from 'carbon-icons-svelte'
   import Chart from 'chart.js/auto'
 
   import { calculatePercentage, formatTime, mebibytesToGigabytes, millicoresToCores } from '../helpers'
@@ -13,6 +13,8 @@
   import { chartData, chartOptions } from './chart'
 
   import './styles.postcss'
+
+  import { goto } from '$app/navigation'
 
   let clusterData: ClusterData = {
     totalPods: 0,
@@ -125,164 +127,180 @@
     </div>
   </div>
 
-  <div class="bg-white dark:bg-gray-800 w-full relative shadow-md sm:rounded-lg overflow-hidden mt-10 px-6">
-    <!-- Header which has Title, Dropdown and Search-->
-    <div class="py-6 dark:border-gray-700 flex items-start">
-      <div class="w-7/12 flex">
-        <div class="flex h-6 items-center space-x-1">
-          <h5 class="dark:text-white font-semibold justify-items-start">Event Logs</h5>
+  <div class="flex flex-col">
+    <div class="bg-white dark:bg-gray-800 w-full relative shadow-md rounded-t-lg overflow-hidden mt-10 px-6">
+      <!-- Header which has Title, Dropdown and Search-->
+      <div class="py-6 dark:border-gray-700 flex items-start">
+        <div class="w-7/12 flex">
+          <div class="flex h-6 items-center space-x-1">
+            <h5 class="dark:text-white font-semibold justify-items-start">Event Logs</h5>
 
-          <div data-tooltip-target="results-tooltip">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-4 w-4 text-gray-400"
-              viewbox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                clip-rule="evenodd"
-              />
-            </svg>
-
-            <span class="sr-only">More info</span>
-
-            <div
-              id="results-tooltip"
-              role="tooltip"
-              class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
-            >
-              Showing 1-10 of 6,560 results
-              <div class="tooltip-arrow" data-popper-arrow=""></div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="w-5/12 flex space-x-3">
-        <div class="w-1/3">
-          <button
-            id="filterDropdownButton"
-            data-dropdown-toggle="filterDropdown"
-            class="w-full flex items-center justify-center py-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-            type="button"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-              class="h-4 w-4 mr-2 text-gray-400"
-              viewbox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z"
-                clip-rule="evenodd"
-              />
-            </svg>
-            Filter
-            <svg
-              class="-mr-1 ml-1.5 w-5 h-5"
-              fill="currentColor"
-              viewbox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                clip-rule="evenodd"
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-              />
-            </svg>
-          </button>
-
-          <div id="filterDropdown" class="z-10 hidden w-48 p-3 bg-white rounded-lg shadow dark:bg-gray-700">
-            <h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-white">Choose brand</h6>
-            <ul class="space-y-2 text-sm" aria-labelledby="filterDropdownButton">
-              <li class="flex items-center">
-                <input
-                  id="apple"
-                  type="checkbox"
-                  value=""
-                  class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+            <div data-tooltip-target="results-tooltip">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-4 w-4 text-gray-400"
+                viewbox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                  clip-rule="evenodd"
                 />
-                <label for="apple" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">Apple (56)</label>
-              </li>
-              <li class="flex items-center">
-                <input
-                  id="fitbit"
-                  type="checkbox"
-                  value=""
-                  class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                />
-                <label for="fitbit" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
-                  Microsoft (16)
-                </label>
-              </li>
-              <li class="flex items-center">
-                <input
-                  id="razor"
-                  type="checkbox"
-                  value=""
-                  class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                />
-                <label for="razor" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">Razor (49)</label>
-              </li>
-              <li class="flex items-center">
-                <input
-                  id="nikon"
-                  type="checkbox"
-                  value=""
-                  class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                />
-                <label for="nikon" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">Nikon (12)</label>
-              </li>
-              <li class="flex items-center">
-                <input
-                  id="benq"
-                  type="checkbox"
-                  value=""
-                  class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                />
-                <label for="benq" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">BenQ (74)</label>
-              </li>
-            </ul>
-          </div>
-        </div>
+              </svg>
 
-        <div class="w-2/3">
-          <form class="flex items-center">
-            <label for="simple-search" class="sr-only">Search</label>
-            <div class="relative w-full">
-              <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg
-                  aria-hidden="true"
-                  class="w-5 h-5 text-gray-500 dark:text-gray-400"
-                  fill="currentColor"
-                  viewbox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
+              <span class="sr-only">More info</span>
+
+              <div
+                id="results-tooltip"
+                role="tooltip"
+                class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+              >
+                Showing 1-10 of 6,560 results
+                <div class="tooltip-arrow" data-popper-arrow=""></div>
               </div>
-
-              <input
-                type="text"
-                id="simple-search"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Search"
-                required
-              />
             </div>
-          </form>
+          </div>
+        </div>
+
+        <div class="w-5/12 flex space-x-3">
+          <div class="w-1/3">
+            <button
+              id="filterDropdownButton"
+              data-dropdown-toggle="filterDropdown"
+              class="w-full flex items-center justify-center py-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              type="button"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                class="h-4 w-4 mr-2 text-gray-400"
+                viewbox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+              Filter
+              <svg
+                class="-mr-1 ml-1.5 w-5 h-5"
+                fill="currentColor"
+                viewbox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  clip-rule="evenodd"
+                  fill-rule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                />
+              </svg>
+            </button>
+
+            <div id="filterDropdown" class="z-10 hidden w-48 p-3 bg-white rounded-lg shadow dark:bg-gray-700">
+              <h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-white">Choose brand</h6>
+              <ul class="space-y-2 text-sm" aria-labelledby="filterDropdownButton">
+                <li class="flex items-center">
+                  <input
+                    id="apple"
+                    type="checkbox"
+                    value=""
+                    class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                  />
+                  <label for="apple" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                    Apple (56)
+                  </label>
+                </li>
+                <li class="flex items-center">
+                  <input
+                    id="fitbit"
+                    type="checkbox"
+                    value=""
+                    class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                  />
+                  <label for="fitbit" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                    Microsoft (16)
+                  </label>
+                </li>
+                <li class="flex items-center">
+                  <input
+                    id="razor"
+                    type="checkbox"
+                    value=""
+                    class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                  />
+                  <label for="razor" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                    Razor (49)
+                  </label>
+                </li>
+                <li class="flex items-center">
+                  <input
+                    id="nikon"
+                    type="checkbox"
+                    value=""
+                    class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                  />
+                  <label for="nikon" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                    Nikon (12)
+                  </label>
+                </li>
+                <li class="flex items-center">
+                  <input
+                    id="benq"
+                    type="checkbox"
+                    value=""
+                    class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                  />
+                  <label for="benq" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">BenQ (74)</label>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="w-2/3">
+            <form class="flex items-center">
+              <label for="simple-search" class="sr-only">Search</label>
+              <div class="relative w-full">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <svg
+                    aria-hidden="true"
+                    class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                    fill="currentColor"
+                    viewbox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </div>
+
+                <input
+                  type="text"
+                  id="simple-search"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Search"
+                  required
+                />
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
+  </div>
+
+  <!-- Footer with link-->
+  <div class="bg-white dark:bg-gray-800 rounded-b-lg px-10 h-20 flex items-center justify-end">
+    <button class="text-sm dark:text-blue-300 flex items-center space-x-1" on:click={() => goto('/monitor/events')}>
+      <span>VIEW EVENTS</span>
+      <ChevronRight />
+    </button>
   </div>
 </div>
