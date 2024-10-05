7ba2599edf63d1d88eb14e9688791d12774b7028
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2024-10-05 09:03:34 -0600

    fix cluster check toast unit tests

--- ui/src/lib/utils/cluster-check/cluster-check.test.ts
+++ ui/src/lib/utils/cluster-check/cluster-check.test.ts
@@ -10,47 +10,21 @@ import { checkClusterConnection } from './cluster-check'
 // Vitest type redeclared cause it's not exported from vitest
 type Procedure = (...args: any[]) => any
 
-const urlAssertionMock = vi.fn().mockImplementation((url: string) => {
-  console.log(url)
-})
+const urlAssertionMock = vi.fn()
 
 class ClusterCheckEventSource {
   // Placeholder for the 'onmessage' event handler
   onmessage: ((event: MessageEvent) => void) | null = null
   onerror: ((event: Event) => void) | null = null
 
-  constructor(
-    url: string,
-    urlAssertionMock: Mock<Procedure>,
-    triggers?: { onError?: number; msg?: { success?: number; error?: number; reconnected?: number } },
-  ) {
+  constructor(url: string, urlAssertionMock: Mock<Procedure>, triggers: { msg: string; timer: number }[]) {
     // Used for testing the correct URL was passed to the EventSource
     urlAssertionMock(url)
 
-    const msg = triggers && triggers?.msg
-
-    if (triggers?.onError) {
+    for (const trigger of triggers) {
       setTimeout(() => {
-        this.onerror?.(new Event('error'))
-      }, triggers.onError)
-    }
-
-    if (msg?.success) {
-      setTimeout(() => {
-        this.onmessage?.(new MessageEvent('message', { data: JSON.stringify({ success: 'success' }) }))
-      }, msg.success)
-    }
-
-    if (msg?.error) {
-      setTimeout(() => {
-        this.onmessage?.(new MessageEvent('message', { data: JSON.stringify({ error: 'error' }) }))
-      }, msg.error)
-    }
-
-    if (msg?.reconnected) {
-      setTimeout(() => {
-        this.onmessage?.(new MessageEvent('message', { data: JSON.stringify({ reconnected: 'reconnected' }) }))
-      }, msg.reconnected)
+        this.onmessage?.(new MessageEvent('message', { data: JSON.stringify(trigger.msg) }))
+      }, trigger.timer)
     }
   }
 }
@@ -66,40 +40,21 @@ describe('cluster check', () => {
     vi.useRealTimers()
   })
 
-  test('cluster check initial error then restored', async () => {
-    vi.stubGlobal(
-      'EventSource',
-      vi
-        .fn()
-        .mockImplementationOnce(
-          (url: string) =>
-            new ClusterCheckEventSource(url, urlAssertionMock, { onError: 1000, msg: { reconnected: 2000 } }),
-        ),
-    )
-    checkClusterConnection()
-    expect(urlAssertionMock).toHaveBeenCalledWith('/health')
-    expect(get(toast)).toHaveLength(0)
-
-    vi.advanceTimersByTime(1000)
-    expect(get(toast)).toHaveLength(1)
-    expect(get(toast)[0].message).toBe('Cluster health check failed: no connection')
-
-    vi.advanceTimersByTime(1000)
-    expect(get(toast)).toHaveLength(1)
-    expect(get(toast)[0].message).toBe('Cluster connection restored')
-  })
-
   test('cluster check success, then error, then restored', async () => {
     vi.stubGlobal(
       'EventSource',
       vi.fn().mockImplementationOnce(
         (url: string) =>
-          new ClusterCheckEventSource(url, urlAssertionMock, {
-            msg: { success: 1000, error: 2000, reconnected: 3000 },
-          }),
+          new ClusterCheckEventSource(url, urlAssertionMock, [
+            { msg: 'success', timer: 1000 },
+            { msg: 'error', timer: 2000 },
+            { msg: 'success', timer: 3000 },
+          ]),
       ),
     )
     checkClusterConnection()
+
+    expect(urlAssertionMock).toHaveBeenCalledWith('/health')
 
     vi.advanceTimersByTime(1000)
     expect(get(toast)).toHaveLength(0)
@@ -116,11 +71,13 @@ describe('cluster check', () => {
   test('multiple errors only show one toast', async () => {
     vi.stubGlobal(
       'EventSource',
-      vi
-        .fn()
-        .mockImplementationOnce(
-          (url: string) => new ClusterCheckEventSource(url, urlAssertionMock, { onError: 1000, msg: { error: 2000 } }),
-        ),
+      vi.fn().mockImplementationOnce(
+        (url: string) =>
+          new ClusterCheckEventSource(url, urlAssertionMock, [
+            { msg: 'error', timer: 1000 },
+            { msg: 'error', timer: 2000 },
+          ]),
+      ),
     )
     checkClusterConnection()
 
@@ -137,12 +94,13 @@ describe('cluster check', () => {
     const eventSpy = vi.spyOn(window, 'dispatchEvent')
     vi.stubGlobal(
       'EventSource',
-      vi
-        .fn()
-        .mockImplementationOnce(
-          (url: string) =>
-            new ClusterCheckEventSource(url, urlAssertionMock, { onError: 1000, msg: { reconnected: 2000 } }),
-        ),
+      vi.fn().mockImplementationOnce(
+        (url: string) =>
+          new ClusterCheckEventSource(url, urlAssertionMock, [
+            { msg: 'error', timer: 1000 },
+            { msg: 'success', timer: 2000 },
+          ]),
+      ),
     )
     checkClusterConnection()
 
