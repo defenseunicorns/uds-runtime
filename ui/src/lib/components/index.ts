5542e47ef27e8b2af843c8b79d5128aa8fd3c892
Billy Figueroa <billy@defenseunicorns.com>
2024-10-08 21:44:45 -0400

    fix: removing core services

--- ui/src/lib/components/index.ts
+++ ui/src/lib/components/index.ts
@@ -2,7 +2,6 @@
 // SPDX-FileCopyrightText: 2024-Present The UDS Authors
 
 export { default as AnsiDisplay } from './AnsiDisplay/component.svelte'
-export { default as CoreServicesWidget } from './CoreServicesWidget/component.svelte'
 export { default as Card } from './k8s/Card/component.svelte'
 export { default as DataTable } from './k8s/DataTable/component.svelte'
 export { default as Drawer } from './k8s/Drawer/component.svelte'
