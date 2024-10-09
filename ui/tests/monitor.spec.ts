1eefd23cb5ba9a6ec0e6bdae896f957db9ab5c69
unclegedd <gedd@defenseunicorns.com>
2024-10-09 10:36:17 -0500

    fixing tests

--- ui/tests/monitor.spec.ts
+++ ui/tests/monitor.spec.ts
@@ -5,12 +5,39 @@ import * as fs from 'node:fs'
 
 import { expect, test } from '@playwright/test'
 
-test.describe('Monitor', async () => {
+// Run monitor tests in serial to ensure we capture the un-cached response time in the first test
+test.describe.serial('Monitor', async () => {
+  // used to store the initial load time of the page before the cache is used
+  let initialLoadTime: number
+
   test.beforeEach(async ({ page }) => {
+    const startTime = new Date().getTime()
     await page.goto('/monitor/pepr')
 
     // wait for data to load
     await page.waitForSelector('.pepr-event.ALLOWED')
+
+    const endTime = new Date().getTime()
+
+    // initial load time is the time to load the data when the cache isn't used (ie. the first visit to the page)
+    initialLoadTime = endTime - startTime
+  })
+
+  test('Pepr cache', async ({ page }) => {
+    // reload the page
+    await page.reload()
+
+    // wait for data to load
+    const startTime = new Date().getTime()
+    await page.waitForSelector('.pepr-event.ALLOWED')
+    const endTime = new Date().getTime()
+    const cachedLoadTime = endTime - startTime
+
+    // adding debug logs to help diagnose test failures
+    console.debug('Initial load time:', initialLoadTime)
+    console.debug('Cached load time:', cachedLoadTime)
+
+    expect(cachedLoadTime).toBeLessThan(initialLoadTime)
   })
 
   test('searching with "All Pepr Events selected', async ({ page }) => {
@@ -83,34 +110,5 @@ test.describe('Monitor', async () => {
       const fileContents = JSON.parse(data)
       expect(fileContents.length).toBeGreaterThan(0)
     })
-  })
-
-  test('Pepr cache', async ({ page }) => {
-    await page.goto('/monitor/pepr')
-
-    // wait for data to load
-    let startTime = new Date().getTime()
-    await page.waitForSelector('.pepr-event.ALLOWED')
-    let endTime = new Date().getTime()
-
-    // initial load time is the time to load the data when the cache isn't used (ie. the first visit to the page)
-    const initialLoadTime = endTime - startTime
-
-    // reload the page
-    await page.reload()
-
-    // wait for data to load
-    startTime = new Date().getTime()
-    await page.waitForSelector('.pepr-event.ALLOWED')
-    endTime = new Date().getTime()
-
-    // cached load time is the time to load the data when the cache is used
-    const cachedLoadTime = endTime - startTime
-
-    // adding debug logs to help diagnose test failures
-    console.debug('Initial load time:', initialLoadTime)
-    console.debug('Cached load time:', cachedLoadTime)
-
-    expect(cachedLoadTime).toBeLessThan(initialLoadTime)
   })
 })
