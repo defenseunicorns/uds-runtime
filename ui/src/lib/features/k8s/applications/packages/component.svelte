9af29e912bb8ae87697c5a0bf2d69f894307d6e6
Billy Figueroa <billy@defenseunicorns.com>
2024-10-15 10:22:57 -0400

    refactor: updating application routes and tests

--- ui/src/lib/features/k8s/applications/packages/component.svelte
+++ ui/src/lib/features/k8s/applications/packages/component.svelte
@@ -7,13 +7,13 @@
   import { createStore, type Columns } from './store'
 
   export let columns: Columns = [
-    ['name', 'emphasize'],
-    ['version'],
-    ['description', 'italic'],
-    ['arch'],
-    ['flavor'],
-    ['components'],
-    ['age'],
+    ['name', 'emphasize w-1/12 truncate'],
+    ['version', ' w-1/12 truncate'],
+    ['description', 'italic  w-5/12 truncate'],
+    ['arch', ' w-1/12 truncate'],
+    ['flavor', ' w-1/12 truncate'],
+    ['components', ' w-2/12 truncate'],
+    ['age', ' w-1/12 truncate'],
   ]
   const name = 'Application Packages'
   const description = 'Packages are either UDS or Zarf packages that are deployed in the cluster.'
