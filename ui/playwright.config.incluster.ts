71d541bc7825cd71eae4e896f5c9a9f3e21fbf3f
Darcy Cleaver <darcy@defenseunicorns.com>
2024-10-15 15:00:42 -0600

    potential new playwright config for incluster

---
+++ ui/playwright.config.incluster.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from '@playwright/test'
+import { loadEnv } from 'vite'
+
+const { VITE_PORT_ENV } = loadEnv('dev', process.cwd())
+
+// use port 8443 because by default we use TLS when running locally
+const port = VITE_PORT_ENV ?? '8443'
+const protocol = 'https'
+const host = 'runtime-local.uds.dev'
+
+export default defineConfig({
+  webServer: {
+    url: `${protocol}://${host}:${port}`,
+  },
+  timeout: 10 * 1000,
+  testDir: 'tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  retries: process.env.CI ? 2 : 1,
+  testMatch: /^(?!.*api-auth)(.+\.)?(test|spec)\.[jt]s$/,
+  use: {
+    baseURL: `${protocol}://${host}:${port}/`,
+  },
+})
+
+export { port }
