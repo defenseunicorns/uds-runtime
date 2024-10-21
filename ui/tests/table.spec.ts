0bcd51ea948eab69690d67fa3eacd96b62ae8a03
Darcy Cleaver <darcy@defenseunicorns.com>
2024-10-21 13:25:24 -0600

    simplify in cluster e2e tests

--- ui/tests/table.spec.ts
+++ ui/tests/table.spec.ts
@@ -1,9 +1,7 @@
 // Copyright 2024 Defense Unicorns
 // SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
 
-import { expect, test } from '@playwright/test'
-
-const isInCluster = process.env.RUNTIME_ENVIRONMENT === 'in-cluster'
+import { expect, test, type Page } from '@playwright/test'
 
 test.describe('DataTable', async () => {
   test.beforeEach(async ({ page }) => {
@@ -12,83 +10,57 @@ test.describe('DataTable', async () => {
 
   test('filters rows when we click the namespace link in a row', async ({ page }) => {
     await page.getByRole('button', { name: 'podinfo' }).last().click()
-
-    if (isInCluster) {
-      expect(await page.getByTestId('table-header-results').textContent()).toBe('(showing 1 of 20)')
-    } else {
-      expect(await page.getByTestId('table-header-results').textContent()).toBe('(showing 1 of 8)')
-    }
-
+    await checkTableHeaderResults(page, 1, 8)
     await page.getByTestId('table-filter-namespace-select').selectOption({ label: 'All Namespaces' })
-
     await page.getByRole('button', { name: 'kube-system' }).first().click()
-
-    if (isInCluster) {
-      expect(await page.getByTestId('table-header-results').textContent()).toBe('(showing 1 of 20)')
-    } else {
-      expect(await page.getByTestId('table-header-results').textContent()).toBe('(showing 3 of 8)')
-    }
+    await checkTableHeaderResults(page, 1, 8)
   })
 
   test('filters rows when we select the namespace from the drop down option', async ({ page }) => {
     await page.getByTestId('table-filter-namespace-select').selectOption({ label: 'podinfo' })
-
-    if (isInCluster) {
-      await page.waitForSelector('[data-testid="table-header-results"]:has-text("(showing 1 of 20)")')
-    } else {
-      await page.waitForSelector('[data-testid="table-header-results"]:has-text("(showing 1 of 8)")')
-    }
-
+    await checkTableHeaderResults(page, 1, 8)
     await page.getByTestId('table-filter-namespace-select').selectOption({ label: 'kube-system' })
-
-    if (isInCluster) {
-      expect(await page.getByTestId('table-header-results').textContent()).toBe('(showing 1 of 20)')
-    } else {
-      expect(await page.getByTestId('table-header-results').textContent()).toBe('(showing 3 of 8)')
-    }
+    await checkTableHeaderResults(page, 1, 8)
   })
 
   test('filters rows when entering search values with "Anywhere" selected', async ({ page }) => {
     await page.getByTestId('datatable-search').fill('pepr')
-
-    if (isInCluster) {
-      await page.waitForSelector('[data-testid="table-header-results"]:has-text("(showing 7 of 20)")')
-    } else {
-      await page.waitForSelector('[data-testid="table-header-results"]:has-text("(showing 4 of 8)")')
-    }
-
+    await checkTableHeaderResults(page, 4, 8)
     await page.getByTestId('datatable-search').fill('podinfo')
-
-    if (isInCluster) {
-      expect(await page.getByTestId('table-header-results').textContent()).toBe('(showing 9 of 20)')
-    } else {
-      expect(await page.getByTestId('table-header-results').textContent()).toBe('(showing 1 of 8)')
-    }
+    await checkTableHeaderResults(page, 1, 8)
   })
 
   test('filters rows when entering search values with "Metadata" selected', async ({ page }) => {
     await page.getByTestId('datatable-filter-dropdown').click()
     await page.getByLabel('Metadata').click()
-
     await page.getByTestId('datatable-search').fill('pepr')
-
-    if (isInCluster) {
-      await page.waitForSelector('[data-testid="table-header-results"]:has-text("(showing 7 of 20)")')
-    } else {
-      await page.waitForSelector('[data-testid="table-header-results"]:has-text("(showing 4 of 8)")')
-    }
+    await checkTableHeaderResults(page, 4, 8)
   })
 
   test('filters rows when entering search values with "Name" selected', async ({ page }) => {
     await page.getByTestId('datatable-filter-dropdown').click()
     await page.getByLabel('Name').click()
-
     await page.getByTestId('datatable-search').fill('pepr')
-
-    if (isInCluster) {
-      await page.waitForSelector('[data-testid="table-header-results"]:has-text("(showing 3 of 20)")')
-    } else {
-      await page.waitForSelector('[data-testid="table-header-results"]:has-text("(showing 3 of 8)")')
-    }
+    await checkTableHeaderResults(page, 3, 8)
   })
 })
+
+// Checks to see if the table header results are as expected. For example: "Showing 1 of 8"
+async function checkTableHeaderResults(page: Page, expectedActual: number, expectedTotal: number) {
+  let tableHeaderResults = page.getByTestId('table-header-results')
+  await tableHeaderResults.waitFor()
+  let textContent = await tableHeaderResults.textContent()
+
+  console.log('textContent:', textContent)
+  const regex = /showing (\d+) of (\d+)/
+  const match = textContent?.match(regex)
+  if (match) {
+    const actual = parseInt(match[1], 10)
+    const total = parseInt(match[2], 10)
+
+    expect(actual).toBeGreaterThanOrEqual(expectedActual)
+    expect(total).toBeGreaterThanOrEqual(expectedTotal)
+  } else {
+    throw new Error('Failed to extract numbers from table header results')
+  }
+}
