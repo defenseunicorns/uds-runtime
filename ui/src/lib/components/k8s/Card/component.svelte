3efcd0eefc48ac9074714f98cc3e061396fca6ac
Billy Figueroa <billy@defenseunicorns.com>
2024-10-22 21:50:13 -0400

    feat(ui): update overview cards to navigate on click

--- ui/src/lib/components/k8s/Card/component.svelte
+++ ui/src/lib/components/k8s/Card/component.svelte
@@ -2,14 +2,17 @@
 <!-- SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial -->
 
 <script lang="ts">
+  import { goto } from '$app/navigation'
   import type { TailwindSizeType } from '$components/StatsWidget/types'
 
   export let size: TailwindSizeType = 32
+  export let link: string = ''
 </script>
 
-<div
+<button
+  on:click={() => goto(link)}
   data-testid="card-container"
   class={`bg-white dark:bg-gray-800 overflow-hidden shadow rounded-lg hover:dark:bg-gray-700 h-${size} px-5 sm:px-6 flex items-center`}
 >
   <slot />
-</div>
+</button>
