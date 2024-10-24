2f6f4a06f8d74b9217e4ccdd91f20303e921973a
unclegedd <gedd@defenseunicorns.com>
2024-10-23 23:07:44 -0500

    regex things

--- ui/playwright.config.local-auth.ts
+++ ui/playwright.config.local-auth.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   testDir: 'tests',
   fullyParallel: false,
   retries: process.env.CI ? 2 : 1,
-  testMatch: /^(?!.*local-auth|.*reconnect|.*in-cluster)(.+\.)?(test|spec)\.[jt]s/,
+  testMatch: /^(?!.*in-cluster|.*reconnect)(.+\.)?(test|spec)\.[jt]s$/,
   use: {
     baseURL: `https://runtime-local.uds.dev:${port}/`,
   },
